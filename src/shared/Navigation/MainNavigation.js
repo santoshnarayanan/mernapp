22a5c810304d2c8755cc9cbd7730f20b508af026
santosh33 <santosh33@gmail.com>
2022-07-12 18:48:33 +0530

    Created links

--- src/shared/Navigation/MainNavigation.js
+++ src/shared/Navigation/MainNavigation.js
@@ -2,20 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
-import './MainNavigation.css';
+import Navlinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import "./MainNavigation.css";
 
-const MainNavigation  = props => {
-    return <MainHeader>
+const MainNavigation = (props) => {
+  return (
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <Navlinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
         <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
+          <span />
+          <span />
+          <span />
         </button>
         <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
+          <Link to="/">Your Places</Link>
         </h1>
-        <nav>...</nav>
-    </MainHeader>
+        <nav className="main-navigation__header-nav">
+          <Navlinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
 };
 
 export default MainNavigation;
