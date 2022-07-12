ee61711994044df62a6e625e904dc8ca234efd49
santosh33 <santosh33@gmail.com>
2022-07-12 15:28:43 +0530

    Updating UserItem Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,9 +7,9 @@ const Users = ()=> {
         id: "u1",
         name: "Max Schar",
         image:
-          "https://images.pexels.com/photos/7014269/pexels-photo-7014269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         places: 3,
-      },
+      }
     ];
     return(
         <UsersList items={USERS} />
