030aa0a555f8df4812722e41d39a007b2309d3b0
santosh33 <santosh33@gmail.com>
2022-07-12 15:12:58 +0530

    Adding new components and styling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,8 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = ()=> {
+    const USERS = [
+      {
+        id: "u1",
+        name: "Max Schar",
+        image:
+          "https://images.pexels.com/photos/7014269/pexels-photo-7014269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        places: 3,
+      },
+    ];
     return(
-        <h2>Users working!</h2>
+        <UsersList items={USERS} />
     );
 };
 
