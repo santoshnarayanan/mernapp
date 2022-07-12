2f905d7ed1554ec8a8aaf30a91dc324c201e1887
santosh33 <santosh33@gmail.com>
2022-07-12 17:38:04 +0530

    Adding Header  & other compnents

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import {
 } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/Navigation/MainNavigation";
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" excat element={<Users />} />
-        <Route path="/places/new" excat element={<NewPlace />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" excat element={<Users />} />
+          <Route path="/places/new" excat element={<NewPlace />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
