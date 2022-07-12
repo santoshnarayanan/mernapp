c9022d28619ba20506f1f63693c233b25937771e
santosh33 <santosh33@gmail.com>
2022-07-12 22:49:27 +0530

    Render UsrPlaces & using dynamic Route Segment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/Navigation/MainNavigation";
 import "./App.css";
 
@@ -17,6 +18,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" excat element={<Users />} />
+          <Route path="/:userId/places" exact element={<UserPlaces />} />
           <Route path="/places/new" excat element={<NewPlace />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+    {
+        id:'p1',
+        title:'Empire State Building',
+        description:'One of the most famous sky scrapers in the world',
+        imageUrl:'https://images.unsplash.com/photo-1555109307-f7d9da25c244?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW1waXJlJTIwc3RhdGUlMjBidWlsZGluZ3xlbnwwfHwwfHw%3D&w=1000&q=80',
+        address:'20 W 34th St., New York, NY 10001',
+        location:{
+            lat:40.7484405,
+            lng:-73.9856644
+        },
+        creator:'u1'
+    },
+    {
+        id:'p2',
+        title:'Burj Khalifa',
+        description:'One of the most famous sky scrapers in Dubai',
+        imageUrl:'https://lh5.googleusercontent.com/p/AF1QipO2Ij2qKRmKpCPKC7Y6WF6nJVet3C7Mfc0W8sMf=w408-h291-k-no',
+        address:'1 Sheikh Mohammed bin Rashid Blvd - Downtown Dubai',
+        location:{
+            lat:25.197197,
+            lng:55.2721877
+        },
+        creator:'u2'
+    }
+]
+
+const UserPlaces = () => {
+    
+    return <PlaceList items={DUMMY_PLACES}/>
+};
+
+export default UserPlaces;
